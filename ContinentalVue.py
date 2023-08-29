1808bd152eb4a2f0cf65c9e2510177507d5d028d
Ada Okafor <adagokafor@gmail.com>
2023-08-29 19:06:39 +0100

    Add files via upload

--- ContinentalVue.py
+++ ContinentalVue.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import streamlit as st
 from PIL import Image
+import matplotlib.pyplot as plt
 import plotly.express as px
+DATA_URL = 'tweets.xlsx'
 
 st.markdown("<h1 style='text-align: center; color: black;'>Explore The Continents</h1>", unsafe_allow_html=True)
 
